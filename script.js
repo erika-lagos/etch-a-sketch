e0aa7d08c167ec67a7fdfbbf133ebd42e30c8ff7
erika-lagos <144739255+erika-lagos@users.noreply.github.com>
2023-09-25 13:24:28 -0700

    Complete basic drawing with cursor

--- script.js
+++ script.js
@@ -12,11 +12,25 @@ function createRow(width) {
     const row = document.createElement('div');
     row.classList.add('row');
     for (let i = 0; i < width; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
+        const square = createSquare();
         row.appendChild(square);
     }
     return row;
+}
+
+function createSquare() {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('mouseover', e => {
+        // console.log("mouse over");
+        e.target.classList.remove('transparent')
+        e.target.classList.add('solid');
+    });
+    square.addEventListener('mouseout', e => {
+        e.target.classList.remove('solid')
+        e.target.classList.add('transparent');
+    });
+    return square;
 }
 
 function init() {
